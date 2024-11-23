07676dd5a48e1dcea83def6a928e0042c664fb07
Anuj Kutal <anxxuj@gmail.com>
2024-11-23 08:47:02 +0530

    handle partial updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -99,10 +99,10 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	// Read JSON request body data sent by client.
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -113,10 +113,18 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	// Copy the values from the request body to
 	// movie and validate the input data.
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
