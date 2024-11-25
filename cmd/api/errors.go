f8786d2bcd2a692943966d489cbc3ac31eaf871d
Anuj Kutal <anxxuj@gmail.com>
2024-11-25 18:08:59 +0530

    create ip-based rate limiter middleware to restrict clients from making too many request too quickly

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -51,3 +51,8 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 	messgae := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, messgae)
 }
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
