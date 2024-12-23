a2a763e345b356e106e74f0edb8e496ff1648ebb
Anuj Kutal <anxxuj@gmail.com>
2024-12-23 10:37:00 +0530

    custom json decoder for Runtime type

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,16 +1,43 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Represents movie runtime
 type Runtime int32
 
-// Return the JSON-encoded value for the movie runtime in the format "<runtime> mins"
+// Convert int32 into string in "<runtime> mins" format
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// Convert string "<runtime> mins" into int32
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the string to isolate the part containing the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Check if string is in expected format
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	return nil
 }
